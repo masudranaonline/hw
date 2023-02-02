1f8738d3d62bce03cab3c06759f1328bc7aef19b
Masud Rana <masudranaonline75@gmail.com>
2023-02-02 15:51:57 +0600

    class 5 uploaded

---
+++ class 5/area_of_the_circle.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    define('PI',3.1416);
+
+    $r = 5;
+
+    $area = PI *$r * $r;
+
+    echo "Area of the circle : ".$area."<br>";
+
+    $circumference = 2 * PI * $r;
+
+    echo "circumference : ".$circumference;
+
---
+++ class 5/area_of_the_triangle.php
@@ -0,0 +1,16 @@
+<?php
+
+    $height = 10;
+    $base = 12;
+
+    $area = 0.5 * $height * $base;
+
+    echo "Area of the triangle : ".$area;
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+   $b = (2 * $area ) / $height;
+
+   echo $b;
