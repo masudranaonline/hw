3f34c3a881a2b1399833f052a63a1aa0092011f1
Masud Rana <masudranaonline75@gmail.com>
2023-05-09 17:39:30 +0600

    class $object

--- practices/class.php
+++ practices/class.php
@@ -20,7 +20,6 @@ use calculation as GlobalCalculation;
              $sum = $this->sum(9,3);
             $this->n1 = $n1;
             $this->n2 = $n2;
-           
             return $result =  $sum - $this->n2;
         }
 
