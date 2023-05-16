8236bfded8b59cfa92caa114943722fa59cc866a
Masud Rana <masudranaonline75@gmail.com>
2023-05-16 23:34:05 +0600

    interface

---
+++ class 29/interface.php
@@ -0,0 +1,20 @@
+<?php
+    interface Calculation{
+        public function sum($n1,$n2);
+        public function sub($n1,$n2);
+    }
+
+    class ChildClass implements Calculation{
+        public function sum($n1,$n2){
+            return $n1+ $n2;
+        }
+        public function sub($n1,$n2){
+            return $n1 - $n2;
+        }
+    }
+
+    $object = new ChildClass();
+   echo $object->sub(10,5);
+
+    // $object = new Calculation();
+
