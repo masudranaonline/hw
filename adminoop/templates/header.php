56e6731f08c1f03bc6f166139b25863f6296e9a3
Masud Rana <masudranaonline75@gmail.com>
2023-08-14 23:42:06 +0600

    oop

--- adminoop/templates/header.php
+++ adminoop/templates/header.php
@@ -49,8 +49,8 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.php" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <img src="assets/img/logo1.png" alt="">
+        <span class="d-none d-lg-block">CPIK</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
